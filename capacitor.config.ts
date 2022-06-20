50be910f78d515047ee6c634c3d43669f93a529f
Christian Saar <c.saar@sigmatech.de>
2022-06-20 10:39:46 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ion-action-sheet-test',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
